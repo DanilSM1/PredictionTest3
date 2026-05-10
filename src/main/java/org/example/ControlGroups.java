1c197bca2d881d2111b51f5f056ef8916e422f2d
Daniil Smirnov <smirnovdanka@gmail.com>
2026-05-10 11:54:16 +0300

    refactoring class ControlGroup with different values of exams

--- src/main/java/org/example/ControlGroups.java
+++ src/main/java/org/example/ControlGroups.java
@@ -7,66 +7,73 @@ import org.json.simple.parser.ParseException;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ControlGroups extends BaseStudentGroup{
-    private final int ex1,ex2,ex3;
 
-    public ControlGroups(int ex1, int ex2, int ex3) {
+    private int[] examScores;
+
+    public ControlGroups(int... examScores) {
         super();
-        this.ex1 = ex1;
-        this.ex2 = ex2;
-        this.ex3 = ex3;
+        this.examScores = examScores.clone();
     }
 
     @Override
     public double avgBall() {
-        return (ex1+ex2+ex3) / 3.0;
+        if(examScores.length == 0)
+            return 0.0;
+        int sum = 0;
+        for (int score : examScores){
+            sum += score;
+        }
+        return sum / (double) examScores.length;
     }
 
     @Override
     public int[] getScores() {
-        return new int[]{ex1,ex2,ex3};
+        return examScores.clone();
     }
 
     @Override
     public String toString() {
         return "ControlGroups{" +
-                "ex1=" + ex1 +
-                ", ex2=" + ex2 +
-                ", ex3=" + ex3 +
+                "examScores=" + Arrays.toString(examScores) +
                 '}';
     }
-    public static ControlGroups[] parseControlGroup(String fileName) {
+
+    public static ControlGroups[] parseControlGroup(String fileName){
+        ControlGroups[] groups = null;
         JSONParser parser = new JSONParser();
-        ControlGroups[] controlGroup = null;
-        try (FileReader reader = new FileReader(fileName)) {
+
+        try(FileReader reader = new FileReader(fileName)){
             JSONArray jsonArray = (JSONArray) parser.parse(reader);
-            controlGroup = new ControlGroups[jsonArray.size()];
+            groups = new ControlGroups[jsonArray.size()];
+
             for (int i = 0; i < jsonArray.size(); i++) {
                 JSONObject jsonObject = (JSONObject) jsonArray.get(i);
-                Long ex1long, ex2long, ex3long;
-                ex1long = (Long) jsonObject.get("exam1");
-                ex2long = (Long) jsonObject.get("exam2");
-                ex3long = (Long) jsonObject.get("exam3");
 
-                int ex1, ex2, ex3;
-                ex1 = ex1long.intValue();
-                ex2 = ex2long.intValue();
-                ex3 = ex3long.intValue();
-                controlGroup[i] = new ControlGroups(ex1, ex2, ex3);
-//                System.out.println(controlGroup[i]);
+                List<Integer> scoreList = new ArrayList<>();
+                for(Object key: jsonObject.keySet()){
+                    String keyStr = (String) key;
+                    if(keyStr.startsWith("exam")){
+                        Long scoreLong = (Long) jsonObject.get(keyStr);
+                        scoreList.add(scoreLong.intValue());
+                    }
+                }
+                int[] scores = new int[scoreList.size()];
+                for(int j = 0; j < scoreList.size(); j++){
+                    scores[j] = scoreList.get(j);
+                }
+                groups[i] = new ControlGroups(scores);
             }
-            System.out.println("Успешно загружено " + controlGroup.length + " объектов из файла " + fileName);
-        } catch (IOException e) {
-            System.err.println("Ошибка при чтении файла " + fileName + ": " + e.getMessage());
-            e.printStackTrace();
-        } catch (ParseException e) {
-            System.err.println("Ошибка при парсинге JSON файла " + fileName + ": " + e.getMessage());
-            e.printStackTrace();
-        } catch (ClassCastException e) {
-            System.err.println("Ошибка приведения типов в файле " + fileName);
+
+            System.out.println("Успешно загружено " + groups.length +
+                    " объктов из файла " + fileName);
+        } catch (IOException | ParseException e){
             e.printStackTrace();
         }
-        return controlGroup;
+        return groups;
     }
 }
